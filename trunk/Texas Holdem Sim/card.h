d884f0612cabb2d95dae0f3a53683193081b8e0e
jeremypjoseph <jeremypjoseph@f8ef7ea0-8649-0410-979b-77ee78b23d19>
2008-04-02 00:51:27 +0000

    modified card.h, added apvector.h, apvector.cpp, deck.h.

--- trunk/Texas Holdem Sim/card.h
+++ trunk/Texas Holdem Sim/card.h
@@ -1,17 +1,35 @@
 #ifndef CARD_H
 #define CARD_H
 
-#include <vector>
-#include <math>
-#include <string>
 
+#include <iostream>
+#include <string>
+using namespace std;
 
 class Card
 {
 	public:
+		
+		enum suit{spades, hearts, diamonds, clubs};
+		Card();
+		Card(int rank, Suit s);
+		string toString()              const;  // return string version
+        bool SameSuitAs(const Card& c) const;  // true if suit same as c
+        int  GetRank()                 const;  // return rank, 1..13
+        string suitString(Suit s)      const;  // return "spades", "hearts",...
 
 	private:
 
-}
+	    string rankString(int r)       const;  // return "ace", "two", ...
+	
+		int myRank;
+		Suit mySuit;
 
-#endif /* CARD_H */
+};
+
+ostream& operator << (ostream& out, const Card& c);
+bool operator == (const Card& lhs, const Card& rhs);
+    
+
+
+#endif
